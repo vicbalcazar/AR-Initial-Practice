ecb83b459cd14491b246c14f913ba4894cddb879
vicbalcazar <31372817+vicbalcazar@users.noreply.github.com>
2018-09-28 11:39:19 -0700

    Add to repo
    Camera Setup

---
+++ AR-Practice/Assets/Scripts/RotateSphere.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSphere : MonoBehaviour {
+
+    Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+        movement = new Vector3(80, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(movement * Time.deltaTime);
+	}
+}
